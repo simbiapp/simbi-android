9dbdc9d041fdcb6e04da1b22945931860d74277c
rahul kumar <rahulkumar2048@gmail.com>
2015-06-27 21:01:34 +0530

    added toolbar

--- app/src/main/java/org/simbi/simbiapp/MainActivity.java
+++ app/src/main/java/org/simbi/simbiapp/MainActivity.java
@@ -1,16 +1,22 @@
 package org.simbi.simbiapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolBar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolBar);
     }
 
     @Override
